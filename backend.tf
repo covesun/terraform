7fdca2e510075de372579b2531eb9e454fdae74f
covesun <covesun@example.com>
2025-05-12 03:21:14 +0900

    Changes to be committed: 	modified:   backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform"
-    storage_account_name = "stcovesunterraform"
-    container_name       = "tfstate"
-    key                  = "test.tfstate"
+    resource_group_name  = var.resource_prefix
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
   }
 }
